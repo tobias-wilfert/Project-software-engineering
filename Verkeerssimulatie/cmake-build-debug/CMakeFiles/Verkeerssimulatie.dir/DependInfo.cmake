705da3e82afa0d722d01f10cfc2917de32859d42
Tobias Wilfert <s0181439@ad.ua.ac.be>
2019-03-25 13:45:17 +0100

    Fist Working Test

---
+++ Verkeerssimulatie/cmake-build-debug/CMakeFiles/Verkeerssimulatie.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/tobiaswilfert/Desktop/Project-software-engineering/Verkeerssimulatie/Baan.cpp" "/Users/tobiaswilfert/Desktop/Project-software-engineering/Verkeerssimulatie/cmake-build-debug/CMakeFiles/Verkeerssimulatie.dir/Baan.cpp.o"
+  "/Users/tobiaswilfert/Desktop/Project-software-engineering/Verkeerssimulatie/System.cpp" "/Users/tobiaswilfert/Desktop/Project-software-engineering/Verkeerssimulatie/cmake-build-debug/CMakeFiles/Verkeerssimulatie.dir/System.cpp.o"
+  "/Users/tobiaswilfert/Desktop/Project-software-engineering/Verkeerssimulatie/Voertuig.cpp" "/Users/tobiaswilfert/Desktop/Project-software-engineering/Verkeerssimulatie/cmake-build-debug/CMakeFiles/Verkeerssimulatie.dir/Voertuig.cpp.o"
+  "/Users/tobiaswilfert/Desktop/Project-software-engineering/Verkeerssimulatie/XmlParser.cpp" "/Users/tobiaswilfert/Desktop/Project-software-engineering/Verkeerssimulatie/cmake-build-debug/CMakeFiles/Verkeerssimulatie.dir/XmlParser.cpp.o"
+  "/Users/tobiaswilfert/Desktop/Project-software-engineering/Verkeerssimulatie/main.cpp" "/Users/tobiaswilfert/Desktop/Project-software-engineering/Verkeerssimulatie/cmake-build-debug/CMakeFiles/Verkeerssimulatie.dir/main.cpp.o"
+  "/Users/tobiaswilfert/Desktop/Project-software-engineering/Verkeerssimulatie/tinystr.cpp" "/Users/tobiaswilfert/Desktop/Project-software-engineering/Verkeerssimulatie/cmake-build-debug/CMakeFiles/Verkeerssimulatie.dir/tinystr.cpp.o"
+  "/Users/tobiaswilfert/Desktop/Project-software-engineering/Verkeerssimulatie/tinyxml.cpp" "/Users/tobiaswilfert/Desktop/Project-software-engineering/Verkeerssimulatie/cmake-build-debug/CMakeFiles/Verkeerssimulatie.dir/tinyxml.cpp.o"
+  "/Users/tobiaswilfert/Desktop/Project-software-engineering/Verkeerssimulatie/tinyxmlerror.cpp" "/Users/tobiaswilfert/Desktop/Project-software-engineering/Verkeerssimulatie/cmake-build-debug/CMakeFiles/Verkeerssimulatie.dir/tinyxmlerror.cpp.o"
+  "/Users/tobiaswilfert/Desktop/Project-software-engineering/Verkeerssimulatie/tinyxmlparser.cpp" "/Users/tobiaswilfert/Desktop/Project-software-engineering/Verkeerssimulatie/cmake-build-debug/CMakeFiles/Verkeerssimulatie.dir/tinyxmlparser.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../gtest/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Verkeerssimulatie/cmake-build-debug/CMakeFiles/Verkeerssimulatie.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Verkeerssimulatie.dir/main.cpp.o"
+  "CMakeFiles/Verkeerssimulatie.dir/tinystr.cpp.o"
+  "CMakeFiles/Verkeerssimulatie.dir/tinyxml.cpp.o"
+  "CMakeFiles/Verkeerssimulatie.dir/tinyxmlerror.cpp.o"
+  "CMakeFiles/Verkeerssimulatie.dir/tinyxmlparser.cpp.o"
+  "CMakeFiles/Verkeerssimulatie.dir/XmlParser.cpp.o"
+  "CMakeFiles/Verkeerssimulatie.dir/Voertuig.cpp.o"
+  "CMakeFiles/Verkeerssimulatie.dir/Baan.cpp.o"
+  "CMakeFiles/Verkeerssimulatie.dir/System.cpp.o"
+  "Verkeerssimulatie.pdb"
+  "Verkeerssimulatie"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Verkeerssimulatie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
